952dde10905a841444880523a74b3512cb409d61
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-23 14:15:08 +0530

    [Sartaj] : added unordered linkedlist program

---
+++ OrderedLinkedList/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OrderedLinkedList
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            OrderList orderList = new OrderList();
+
+            Console.WriteLine("Enter word to search");
+            string word = Console.ReadLine();
+            orderList.Ordered(word);
+        }
+    }
+}
